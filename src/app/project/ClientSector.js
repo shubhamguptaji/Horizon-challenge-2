bc4f8906cfd20033b5734dbb9b08ae000a5e7500
gurcharan <gurcharan7373@gmail.com>
2019-03-02 11:46:52 +0530

    combining compo

--- src/app/project/ClientSector.js
+++ src/app/project/ClientSector.js
@@ -5,40 +5,32 @@ const CLIENTS_SECTOR = "Client's Sector";
 class Subcomponent extends Component {
   render() {
     return (
-      <div Style={{ marginTop: '40px' }}>
+      <Grid.Row>
         {Array.apply(null, { length: this.props.count }).map((e, i) => (
           <Grid.Row key={i}>
             <Grid.Column width="6">
-              <Form.Input
-                placeholder="Select Sector of Client"
-                type="text"
-                style={{ width: '100%' }}
-              />
-            </Grid.Column>
+                  <Form.Input
+                    placeholder="UNCOVERED"
+                    type="text"
+                    style={{ width: '60%' }}
+                  />
+                </Grid.Column>
+                <Grid.Column width="3">
+                  <Form.Input
+                    type="text"
+                    style={{ width: '50%' }}
+                    value={this.props.value.toFixed(2)}
+                  />
+                  <span>% split</span>
+                  <Icon name="close" onClick={this.props.handleremovesector}></Icon>
+                </Grid.Column>
           </Grid.Row>
         ))}
-      </div>
+      </Grid.Row>
     );
   }
 }
-class Subcomponent2 extends Component {
-  render() {
-    return (
-      <div>
-        {Array.apply(null, { length: this.props.count }).map((e, i) => (
-          <div key={i}>
-            <Form.Input
-              type="text"
-              style={{ width: '100%' }}
-              value={this.props.value}
-            />
-            <Button>% Split</Button>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+
 export default class ClientSector extends Component {
   constructor() {
     super();
@@ -60,14 +52,17 @@ export default class ClientSector extends Component {
     );
   };
   handleaddlocation = () => {
-    this.setState({ reportinglocation: this.state.reportinglocation + 1 });
+    this.setState(
+        { reportinglocation: this.state.reportinglocation + 1 });
   };
   handleremovesector = () => {
-    this.setState({ reportingsector: this.state.reportinglocation - 1 });
+    this.setState({ reportingsector: this.state.reportingsector - 1 },function(){
+        this.setState({value:100/this.state.reportingsector})
+    });
   };
   handleremovelocation = () => {
     this.setState({ reportinglocation: this.state.reportinglocation - 1 });
-  };
+  }
   render() {
     console.log(this.state.reportingsector + 'rendered report');
     return (
@@ -86,48 +81,36 @@ export default class ClientSector extends Component {
           </Grid.Row>
           <Grid.Row>
             <Grid.Column width="3">CS Reporting Sector</Grid.Column>
-            <Grid.Column width="6">
-              <Form.Input
-                placeholder="UNCOVERED"
-                type="text"
-                style={{ width: '100%' }}
-              />
+            <Grid.Column width="10">
+              <Grid.Row>
+                <Grid.Column width="6">
+                  <Form.Input
+                    placeholder="UNCOVERED"
+                    type="text"
+                    style={{ width: '60%' }}
+                  />
+                </Grid.Column>
+                <Grid.Column width="3">
+                  <Form.Input
+                    type="text"
+                    style={{ width: '50%' }}
+                    value={this.state.value.toFixed(2)}
+                  />
+                  <span>%split</span>
+                  
+                </Grid.Column>
+              </Grid.Row>
+
               {this.state.reportingsector > 1 ? (
-                <Subcomponent count={this.state.reportingsector - 1} />
+                <Subcomponent count={this.state.reportingsector - 1} value={this.state.value} handleremovesector={this.handleremovesector} />
               ) : (
                 <div />
               )}
-
-              <Form.Button onClick={this.handleaddsector}>
-                +ADD ANOTHER REPORTING SECTOR
-              </Form.Button>
             </Grid.Column>
             
-            <Grid.Column width="3" >
-              <Grid.Row>
-                <div>
-                  <Form.Input
-                    type="text"
-                    style={{ width: '50%' ,display:"inline-block"}}
-                    value={this.state.value.toFixed(2)}
-                  />
-
-                  <Button >% Split</Button>
-                </div>
-              </Grid.Row>
-
-              <Grid.Row>
-                {console.log(this.state.value + 'value')}
-                {this.state.reportingsector > 1 ? (
-                  <Subcomponent2
-                    count={this.state.reportingsector - 1}
-                    value={this.state.value.toFixed(2)}
-                  />
-                ) : (
-                  <div />
-                )}
-              </Grid.Row>
-            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+              <Button onClick={this.handleaddsector}>+ADD ANTOHER REPORTING SECTOR</Button>
           </Grid.Row>
 
           <Grid.Row>
